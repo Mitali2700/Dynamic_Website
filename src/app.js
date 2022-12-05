eafdb7b546d6a04a2a8068124a30a70302bb7b35
Mitali2700 <Mitali2700>
2022-12-06 01:25:08 +0530

    started creating routes and hbs files

--- src/app.js
+++ src/app.js
@@ -1,2 +1,20 @@
-console.log("project started");
-console.log("project started");
+// const { request, response } = require("express");
+const express = require("express");
+const hbs= require("hbs")
+const app = express()
+
+const routes=require('./routes/main')
+
+//(template engines)
+app.set('view engine','hbs')
+app.set("views","views")
+
+app.use('',routes)
+
+// app.get("/",(request,response)=>{
+//     response.send("Wow this is data from server");
+// })
+
+app.listen(process.env.PORT | 5555,()=>{
+    console.log("server started")
+})
