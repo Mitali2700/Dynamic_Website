01f1166aeb6937cb14ccd7f2d933a479d44b29ca
Mitali2700 <Mitali2700>
2022-12-06 11:10:18 +0530

    database connected

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@
 const express = require("express");
 const hbs= require("hbs")
 const app = express()
+const mongoose = require("mongoose")
 
 const routes=require('./routes/main')
 
+app.use('/static',express.static("public"))
+app.use('',routes)
 //(template engines)
 app.set('view engine','hbs')
 app.set("views","views")
@@ -14,6 +17,11 @@ app.use('',routes)
 // app.get("/",(request,response)=>{
 //     response.send("Wow this is data from server");
 // })
+
+//db connections
+mongoose.connect("mongodb://localhost/website_tut",()=>{
+    console.log("db connected")
+})
 
 app.listen(process.env.PORT | 5555,()=>{
     console.log("server started")
